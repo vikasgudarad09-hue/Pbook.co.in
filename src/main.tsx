d1cb7254f344af7a447528823c3647079dd3794d
Vikas gudarad <vikasgudarad09@gmail.com>
2026-07-30 19:35:45 +0530

    chore: trigger redeployment for environment variables

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Triggering a GitHub sync to redeploy on Vercel with new environment variables
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
